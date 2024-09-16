b5cdaf251d9e600c79845cdc6943f5616a2fbc5f
HimashaNethmini <himashanethmini05@gmail.com>
2024-09-16 10:05:52 +0530

    Make add-appointment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './index.css'
 import { BiArchive } from "react-icons/bi";
 import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-semibold">
       <h1 className='text-5xl'>
         <BiArchive className='inline-block text-blue-800 align-top'/>
-          Your Appointment
+          <AddAppointment />
       </h1> 
       <Search />
     </div>
